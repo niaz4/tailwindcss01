6f8a23fbe4f845f758920467cba0f5de4f1a612d
niaz4 <code.county69@gmail.com>
2022-07-26 18:24:18 +0600

    just commited all the files

---
+++ foodCounty/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+}
